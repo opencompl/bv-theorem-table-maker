039c3861f1998033ab66fbb4e5426a79a0c19629
luisacicolini <lc985@cam.ac.uk>
2024-12-11 17:50:03 +0100

    feat: add printing

--- Main.lean
+++ Main.lean
@@ -5,19 +5,30 @@ Authors: Scott Morrison
 -/
 import Lean.CoreM
 import Lean4Checker.Lean
+-- import Std.Data.HashMap
+import Lean.Data.HashMap
+-- import System.FilePath
+#check System.FilePath
+#check IO.FS.writeFile
+
 
 open Lean
 
 structure Context where
+  -- new constants or whatever.
   newConstants : HashMap Name ConstantInfo
 
 structure State where
+  /-- A Lean environment, which has declarations, theorems, and stuff.-/
   env : Environment
+  -- some kind of BFS
   remaining : NameSet := {}
   pending : NameSet := {}
 
+-- our monad, helpfully named m.
 abbrev M := ReaderT Context <| StateT State IO
 
+-- function to run said monad.
 def M.run (env : Environment) (newConstants : HashMap Name ConstantInfo) (act : M α) : IO α :=
   StateT.run' (s := { env, remaining := newConstants.keyNameSet }) do
     ReaderT.run (r := { newConstants }) do
@@ -65,7 +76,9 @@ partial def replayConstant (name : Name) : M Unit := do
         IO.println s!"Replaying defn {name}"
         addDecl (Declaration.defnDecl   info)
       | .thmInfo    info =>
+        -- Add code here to check what theorem we have or whatever.
         IO.println s!"Replaying thm {name}"
+        -- check if name contains toInt, and if so, record that we have toInt lemma.
         addDecl (Declaration.thmDecl    info)
       | .axiomInfo  info =>
         IO.println s!"Replaying axiom {name}"
@@ -120,14 +133,112 @@ unsafe def diffEnvironments (module : Name) (act : Environment → Environment �
     Lean.withImportModules (after.importsOf module) {} 0 fun before =>
       act before after
 
+def accessorNames : Array String := #[
+    "toInt",
+    "toNat"
+ ]
+
+def functionNames : Array String := #[
+  "signExtend",
+  "add",
+  "sub"
+]
+
+
+open Lean in
+/-- Draw the hashmap as a latex table. -/
+def renderLatexTable (t : HashMap (String × String) Bool) : String := Id.run do
+  let mut out := ""
+  let numRows := functionNames.size
+  out := out.append "\\begin{tabular}"
+  for fn in functionNames do
+    for acc in accessorNames do
+      pure ()
+  out := out.append "\\end{tabular}"
+  return out
+
+/--
+Pad a string to the right to ensure that it uses at least 'amt' space.
+will not pad if length exceeds 'amt'.
+-/
+def padRight (s : String) (amt : Nat) : String :=
+  String.pushn s ' ' (amt - s.length)
+
+/--
+Pad a string to the left to ensure that it uses at least 'amt' space.
+will not pad if length exceeds 'amt'.
+-/
+def padLeft (s : String) (amt : Nat) : String :=
+  let padding := "".pushn ' ' (amt - s.length)
+  padding ++ s
+
+/-- Draw the hashmap as a latex table. -/
+def renderMarkdownTable (t : HashMap (String × String) Bool) : String := Id.run do
+  let mut out := ""
+
+  let accessorPad := accessorNames.foldl (init := 0) fun n acc => Nat.max n acc.length + 2
+  let fnPad := functionNames.foldl (init := 0) fun acc fn => Nat.max acc fn.length + 2
+  let pad := Nat.max fnPad accessorPad
+
+  for fn in #[""] ++ functionNames do
+    out := out.append "|"
+    out := out.append <| padRight fn pad
+
+  for acc in accessorNames do
+    out := out.append "\n"
+    out := out.append "|"
+    out := out.append <| padRight acc pad
+
+    for fn in functionNames do
+      if t.contains (acc, fn) then
+        out := out.append "|"
+        out := out.append <| padRight "✓" pad
+      else
+        out := out.append "|"
+        out := out.append <| padRight "⨯" pad
+  return out
+
+
+partial def _root_.String.containsSubstr? (needle : String) (haystack: String) : Bool :=
+  -- empty string is subtring of itself.
+  if haystack.length == 0 then needle.length == 0
+  else
+    if haystack.isPrefixOf needle
+    then True
+    else containsSubstr? needle (haystack.drop 1)
+
+-- Z-algorithm for string substring matching
+-- Actually linear time, actually easy to implement! Unlike the weirder ones.
+-- Competitive programming reference: https://cp-algorithms.com/string/z-function.html
+-- Good algorithmic references: Algorithms on Strings, Sequences, and Trees [Good explanation]
+-- Sexy algorithmic reference: Jewels of Stringology (Kawaii explanation, not very sensible on first read).
+partial def containsSubstr₂ (needle haystack : String) :  Bool := Id.run do
+  sorry
+
+open Lean in
 unsafe def replay (module : Name) : IO Unit := do
   diffEnvironments module fun before after => do
+    -- please give me all the things that were added.
     let newConstants := after.constants.map₁.toList.filter
       -- We skip unsafe constants, and also partial constants. Later we may want to handle partial constants.
       fun ⟨n, ci⟩ => !before.constants.map₁.contains n && !ci.isUnsafe && !ci.isPartial
-    M.run before (HashMap.ofList newConstants) do
-      for (n, _) in newConstants do
-        replayConstant n
+
+    let mut table : HashMap (String × String) Bool := HashMap.empty
+
+    for (constName, constInfo) in newConstants do
+      -- do our filtering / tabulating / ...
+      IO.println constName
+      if let .thmInfo info := constInfo then
+        for acc in accessorNames do
+          for fn in functionNames do
+            let haveThm? := constName.toString.containsSubstr? acc && constName.toString.containsSubstr? fn
+            table := table.insert (acc, fn) haveThm?
+        IO.println s!"* {constName} is a theorem, with value '{info.value.hash}'"
+
+    IO.FS.writeFile "table.tex" (renderLatexTable table)
+
+    IO.println "---"
+    IO.println (renderMarkdownTable table)
 
 /--
 Run as e.g. `lake exe lean4checker Mathlib.Data.Nat.Basic`.
